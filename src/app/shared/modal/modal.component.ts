62d66b60e45139f63b721c4e96aab234dbb6f958
Claudio Godoy <clouds83@gmail.com>
2022-12-10 17:07:21 -0300

    update

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, ElementRef } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @Input() modalID = ''
+
+  constructor (public modal: ModalService, public el: ElementRef) {}
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
+  }
+
+}
