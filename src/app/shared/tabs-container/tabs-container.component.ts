b382906c55b3289d5af4b5cad5af6db3904aa0c3
Claudio Godoy <clouds83@gmail.com>
2022-12-11 14:27:42 -0300

    update

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.scss']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
+  //@ContentChildren(TabComponent) tabs?: QueryList<TabComponent> *the same as above
+
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs.filter(
+      tab => tab.active
+    )
+
+    if(!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+
+    tab.active = true
+  }
 
 }
