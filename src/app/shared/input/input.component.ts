fae2b35ecc320f6bdb34bc9d3a7d245d355860db
Claudio Godoy <clouds83@gmail.com>
2022-12-11 22:45:52 -0300

    update

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent {
+  @Input() control: FormControl = new FormControl()
+  @Input() type = 'text'
+  @Input() placeholder = ''
+
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +8,27 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]),
+    confirm_password: new FormControl('',[
+      Validators.required,
+      // Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]),
     phoneNumber: new FormControl(''),
   })
 
