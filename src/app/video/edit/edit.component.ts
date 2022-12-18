79610b04a37f9d6a04d724db445b9a8b599e1535
Claudio Godoy <clouds83@gmail.com>
2022-12-18 19:26:52 -0300

    update

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null;
 
+  clipID = new FormControl('', {
+    nonNullable: true,
+  });
+
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID,
+  });
+
+  constructor(private modal: ModalService) {}
+
+  ngOnInit(): void {
+    this.modal.register('editClip');
+  }
+
+  ngOnDestroy(): void {
+    this.modal.unregister('editClip');
+  }
+
+  ngOnChanges(): void {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.clipID.setValue(this.activeClip.docID as string);
+    this.title.setValue(this.activeClip.title);
+  }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -11,11 +12,13 @@ import IClip from 'src/app/models/clip.model';
 export class ManageComponent implements OnInit {
   videoOrder = '1';
   clips: IClip[] = [];
+  activeClip: IClip | null = null;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -44,5 +47,13 @@ export class ManageComponent implements OnInit {
         sort: value,
       },
     });
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.activeClip = clip;
+
+    this.modal.toggleModal('editClip');
   }
 }
