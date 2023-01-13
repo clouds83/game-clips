61a17c5da0cc39dbdad6bd3fa5d354d88ef32bfb
Claudio Godoy <clouds83@gmail.com>
2023-01-13 17:39:23 -0300

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clipz'`, () => {
+  it(`should have as title 'game-clips'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clipz');
+    expect(app.title).toEqual('game-clips');
   });
 
   it('should render title', () => {
